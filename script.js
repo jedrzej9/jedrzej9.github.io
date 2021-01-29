c6e81a30f1570214452930f7e73c812fa27b1153
Michał <46096121+jedrzej9@users.noreply.github.com>
2021-01-29 22:44:12 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,126 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function () {
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    // var typed = new Typed(".typing", {
+    //     strings: ["Developer", "Blogger", "Designer", "Freelancer"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
+
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Software Developer", "Game Developer", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // animation on appearing boxes (expierence section)
+    var items = document.querySelectorAll(".experience li");
+ 
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+      }
+ 
+    function callbackFunc() {
+      for (var i = 0; i < items.length; i++) {
+        if (isElementInViewport(items[i])) {
+          items[i].classList.add("in-view");
+        }
+      }
+    }
+ 
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+
+    // ===== MIXITUP FILTER PROJECTS =====
+    const mixer = mixitup('.projects-items', {
+        selectors: {
+            target: '.card'
+        },
+        animation: {
+            duration: 400
+        }
+    });
+
+    // Link active projects
+    // const linkProjects = document.querySelectorAll('.projects')
+
+    // function activeProjects(){
+    //     if(linkProjects){
+    //         linkProjects.forEach(l => l.classList.remove('active-project'))
+    //         this.classList.add('active-project')
+    //     }
+    // }
+    // linkProjects.forEach(l => l.addEventListener('click', activeProjects))
+
+    // ===== MIXITUP FILTER HOBBIES =====
+    const mixer_hobbies = mixitup('.hobbies-items', {
+        selectors: {
+            target: '.card'
+        },
+        animation: {
+            duration: 400
+        }
+    });
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: false,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
